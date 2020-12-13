e1f17ba9e91889fa2b90e098a9b6604256ca0ff3
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-12-13 12:23:13 +0100

    Fixes PageController tests

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class PageControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get page_index_url
-  #   assert_response :success
-  # end
+  test "shouldn't get index but new user form" do
+    User.destroy_all
+    get page_index_url
+    assert_redirected_to new_user_path
+  end
 
-  # test "should get about_us" do
-  #   get page_about_us_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get page_index_url
+    assert_response :success
+  end
 
+  test "should get about_us" do
+    get page_about_us_url
+    assert_response :success
+  end
 end
