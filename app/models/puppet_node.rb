f93500ab4a1f325e2459e17f87c28802883a993b
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-12-16 08:12:52 +0100

    Checks for version 5.

--- app/models/puppet_node.rb
+++ app/models/puppet_node.rb
@@ -9,19 +9,21 @@ class PuppetNode < ApplicationRecord
     # Read the hiera configuration
     hiera_config = YAML.load_file(PUPPET_CONF_DIR + "/environments/#{self.puppet_environment.name}/hiera.yaml")
 
-    # Read the paths and replaces %{::...}
-    result = []
-    hiera_config["hierarchy"].each do |hierarchy|
-      hierarchy["paths"].each do |hierachy_path|
-        resolved_path = hierachy_path
-        resolved_path = resolved_path.gsub("%{::fqdn}", self.fqdn)
-        resolved_path = resolved_path.gsub("%{::zone}", self.zone)
-        resolved_path = resolved_path.gsub("%{::role}", self.role)
-        resolved_path = resolved_path.gsub("%{::env}", self.puppet_environment.name)
-        result.push(resolved_path)
+    if hiera_config["version"] == 5 # Only works with version 5.
+      # Read the paths and replaces %{::...}
+      result = []
+      hiera_config["hierarchy"].each do |hierarchy|
+        hierarchy["paths"].each do |hierachy_path|
+          resolved_path = hierachy_path
+          resolved_path = resolved_path.gsub("%{::fqdn}", self.fqdn)
+          resolved_path = resolved_path.gsub("%{::zone}", self.zone)
+          resolved_path = resolved_path.gsub("%{::role}", self.role)
+          resolved_path = resolved_path.gsub("%{::env}", self.puppet_environment.name)
+          result.push(resolved_path)
+        end
       end
-    end
 
-    return result
+      return result
+    end
   end
 end
