b050625821460ca5f449aa096bc083f3d27ed09d
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2020-12-16 14:49:59 +0100

    Fixes tests.

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -3,8 +3,16 @@ require 'test_helper'
 class PageControllerTest < ActionDispatch::IntegrationTest
   test "system without an existing user shouldn't get index but new user form" do
     User.destroy_all
+    FactoryBot.create(:role)
     get page_index_url
     assert_redirected_to new_user_path
+  end
+
+  test "system without an existing user nor role shouldn't get index but new user form" do
+    User.destroy_all
+    Role.destroy_all
+    get page_index_url
+    assert_redirected_to page_faulty_setup_path
   end
 
   test "system with existing user should get index" do
