18ece87ea317b8aa189c6d36f8b7ecbb7b20da5d
David Roetzel <david@roetzel.de>
2021-05-07 15:43:19 +0200

    Allow filtering keys #92

---
+++ app/javascript/controllers/key_filter_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "stimulus"
+const Turbolinks = require("turbolinks")
+
+export default class extends Controller {
+  static targets = ["input", "keys"];
+
+  connect() {
+    this.filter();
+  }
+
+  filter() {
+    const search = this.inputTarget.value;
+    this.keysTargets.forEach( keyLink => {
+      if (search == "" || keyLink.innerText.indexOf(search) >= 0) {
+        keyLink.classList.remove("d-none");
+        const url = new URL(keyLink.href);
+        url.searchParams.set('search', search);
+        keyLink.href = url.toString();
+      } else {
+        keyLink.classList.add("d-none");
+      }
+    });
+  }
+
+  submit(event) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// JS Imports
 require("@rails/ujs").start()
 const Turbolinks = require("turbolinks")
 Turbolinks.start()
@@ -10,20 +11,20 @@ require("@rails/activestorage").start()
 require("channels")
 import "controllers"
 
+// Fonts
 import "@fontsource/open-sans/400.css"
 import "@fontsource/open-sans/400-italic.css"
 import "@fontsource/open-sans/700.css"
 
+// Icons
+import "bootstrap-icons/icons/search.svg"
+
+// Stylesheets
 import "bootstrap"
 import "../stylesheets/application"
 
+// Bootstrap JS Initialization
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
