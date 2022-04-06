3835774d3e5cd4ee36dd26ccbd7e16d5bdf6038a
Robert Waffen <rw@betadots.de>
2022-04-06 15:43:05 +0200

    add env var to enable dev mode

--- bin/entry.sh
+++ bin/entry.sh
@@ -4,7 +4,10 @@
 
 bundle exec rails db:create
 bundle exec rails db:migrate
-# bundle exec rails db:seed
 
-# ./bin/fake_puppet_db &
+if [[ $DEVELOP -eq 1 ]]; then
+bundle exec rails db:seed
+./bin/fake_puppet_db &
+fi
+
 bundle exec rails server -b 0.0.0.0
