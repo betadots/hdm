f939984ae54d5beb0a4a55e997e8abf6ab52ea61
David Roetzel <david@roetzel.de>
2023-05-31 15:10:32 +0200

    Display value differences to actual env #151
    If a key is displayed from a different environment
    than the node is actually in _and_ a file with
    the same path exists in the original environment
    _and_ the value in that file differs from the one
    displayed, show the differences.
    
    Uses the `diffy` gem to highlight the differences.

---
+++ test/models/data_file_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class DataFileTest < ActiveSupport::TestCase
+  setup do
+    @original_environment = Environment.new(name: "development")
+    @node = Node.new(environment: @original_environment, hostname: "test.host")
+    @globs_environment = Environment.new(name: "globs")
+    @path = "role/hdm_test.yaml"
+  end
+
+  test "#has_differing_value_in_original_environment? returns false if environments are the same" do
+    hierarchy = Hierarchy.find(@original_environment, "Eyaml hierarchy")
+    data_file = DataFile.new(hierarchy: hierarchy, node: @node, path: @path)
+    key = Key.new(environment: @original_environment, name: "hdm::integer")
+
+    assert_not data_file.has_differing_value_in_original_environment?(key)
+  end
+
+  test "#has_differing_value_in_original_environment? returns false if original environment has no file with the same path" do
+    hierarchy = Hierarchy.find(@globs_environment, "Common")
+    data_file = DataFile.new(hierarchy: hierarchy, node: @node, path: "common/foobar.yaml")
+    key = Key.new(environment: @globs_environment, name: "hdm::integer")
+
+    assert_not data_file.has_differing_value_in_original_environment?(key)
+  end
+
+  test "#has_differing_value_in_original_environment? returns false if original environment's value is the same" do
+    hierarchy = Hierarchy.find(@globs_environment, "Eyaml hierarchy")
+    data_file = DataFile.new(hierarchy: hierarchy, node: @node, path: @path)
+    key = Key.new(environment: @globs_environment, name: "hdm::integer")
+
+    assert_not data_file.has_differing_value_in_original_environment?(key)
+  end
+
+  test "#has_differing_value_in_original_environment? returns true if original environment's value is different" do
+    hierarchy = Hierarchy.find(@globs_environment, "Eyaml hierarchy")
+    data_file = DataFile.new(hierarchy: hierarchy, node: @node, path: @path)
+    key = Key.new(environment: @globs_environment, name: "hdm::float")
+
+    assert data_file.has_differing_value_in_original_environment?(key)
+  end
+
+  test "#value_for returns matching value from given environment" do
+    hierarchy = Hierarchy.find(@globs_environment, "Eyaml hierarchy")
+    data_file = DataFile.new(hierarchy: hierarchy, node: @node, path: @path)
+    key = Key.new(environment: @globs_environment, name: "hdm::float")
+
+    assert_equal "4.5", data_file.value_for(key: key).value
+  end
+
+  test "#value_from_original_environment returns matching value from the node's original environment" do
+    hierarchy = Hierarchy.find(@globs_environment, "Eyaml hierarchy")
+    data_file = DataFile.new(hierarchy: hierarchy, node: @node, path: @path)
+    key = Key.new(environment: @globs_environment, name: "hdm::float")
+
+    assert_equal "2.3", data_file.value_from_original_environment(key: key).value
+  end
+end
