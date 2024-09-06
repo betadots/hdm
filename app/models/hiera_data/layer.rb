7ae27d498403813b616834ae66b7b2c4de9e7b1e
Robert Waffen <rw@betadots.de>
2024-09-06 10:50:01 +0200

    fix: revert back and skip cop for this. prsent is here a custom method
    Signed-off-by: Robert Waffen <rw@betadots.de>

--- app/models/hiera_data/layer.rb
+++ app/models/hiera_data/layer.rb
@@ -1,7 +1,9 @@
 class HieraData
   module Layer
     def self.for(environment:, key: nil)
-      [Global.new, Environment.new(environment:), Module.new(environment:, key:)].compact_blank
+      # rubocop:disable Rails/CompactBlank
+      [Global.new, Environment.new(environment:), Module.new(environment:, key:)].select(&:present?)
+      # rubocop:enable Rails/CompactBlank
     end
   end
 end
