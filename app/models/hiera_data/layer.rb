c71baf7ba287f7b4d24d8e320f41e1ebf69dc392
Robert Waffen <rw@betadots.de>
2024-09-06 10:10:12 +0200

    feat: update rubocop and implement suggestions
    Signed-off-by: Robert Waffen <rw@betadots.de>

--- app/models/hiera_data/layer.rb
+++ app/models/hiera_data/layer.rb
@@ -1,7 +1,7 @@
 class HieraData
   module Layer
     def self.for(environment:, key: nil)
-      [Global.new, Environment.new(environment:), Module.new(environment:, key:)].select(&:present?)
+      [Global.new, Environment.new(environment:), Module.new(environment:, key:)].compact_blank
     end
   end
 end
