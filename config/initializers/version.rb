242cf41fab64867618043a94de7b75b14f14be7a
Robert Waffen <rw@betadots.de>
2025-02-14 16:23:47 +0100

    fix: add elfi if not in container and in git repo

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,5 +1,7 @@
 Hdm::VERSION = if File.exist? '/VERSION'
                  File.read('/VERSION')
+               elsif system("git --version >>/dev/null 2>&1")
+                  `git describe`.strip
                else
                 'unknown'
                end
