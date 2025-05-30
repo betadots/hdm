e04ae64e9fd352c7ecdeff03ce733ba07b352037
David Roetzel <david@roetzel.de>
2025-05-30 12:42:06 +0200

    Enhance ldap configuration (#588, #593)
    This allows setting a different username attribute (e.g. `uid`
    instead of the default, `mail`).
    
    It also allows specifying an additional filter that is added
    when trying to authenticate a user. This can be used to e.g. limit
    ldap authentication to a group (but really anything).
    
    Last but not least, TLS options have been enhanced. "STARTTLS"
    should now be possible, also a CA file can be specified.
    Certificate verification has been turned on as a new default,
    but can optionally be turned off.

--- test/models/ldap_test.rb
+++ test/models/ldap_test.rb
@@ -2,13 +2,46 @@ require 'test_helper'
 
 class LdapTest < ActiveSupport::TestCase
   test "#authenticate delegates to Net::LDAP's #bind_as" do
-    ldap = Ldap.new
-    net_ldap = Minitest::Mock.new
-    ldap.stub(:ldap, net_ldap) do
-      net_ldap.expect(:bind_as, 42, [], filter: String, password: String)
+    stubbed_ldap do |ldap, net_ldap|
+      filter = "(mail=test@example.com)"
+      net_ldap.expect(:bind_as, 42, [], filter:, password: String)
       authentic = ldap.authenticate("test@example.com", "secret")
       assert_equal authentic, 42
       net_ldap.verify
+    end
+  end
+
+  test "#authenticate allows overwriting the username attribute" do
+    Rails.configuration.hdm[:ldap][:username_attribute] = "uid"
+    stubbed_ldap do |ldap, net_ldap|
+      filter = "(uid=testuser)"
+      net_ldap.expect(:bind_as, 42, [], filter:, password: String)
+      authentic = ldap.authenticate("testuser", "secret")
+      assert_equal authentic, 42
+      net_ldap.verify
+    end
+    Rails.configuration.hdm[:ldap].delete(:username_attribute)
+  end
+
+  test "#authenticate uses an extra filter when given" do
+    Rails.configuration.hdm[:ldap][:filter] = "(gid=23)"
+    stubbed_ldap do |ldap, net_ldap|
+      filter = "(&(gid=23)(mail=test@example.com))"
+      net_ldap.expect(:bind_as, 42, [], filter:, password: String)
+      authentic = ldap.authenticate("test@example.com", "secret")
+      assert_equal authentic, 42
+      net_ldap.verify
+    end
+    Rails.configuration.hdm[:ldap].delete(:filter)
+  end
+
+  private
+
+  def stubbed_ldap
+    ldap = Ldap.new
+    net_ldap = Minitest::Mock.new
+    ldap.stub(:ldap, net_ldap) do
+      yield ldap, net_ldap
     end
   end
 end
